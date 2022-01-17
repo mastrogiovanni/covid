273e58bc2f4d8947c0be8c811b39bea0317f716b
Michele Mastrogiovanni <michele.mastrogiovanni@iota.org>
2022-01-17 15:03:09 +0100

    first import

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { Accordion, AccordionItem } from "sveltestrap";
+</script>
+
+<Accordion>
+	<AccordionItem active header="Chi sono i referenti COVID">
+		Sono persone dedicate a gestire le situazioni legate al COVID.<br/>
+		Sono:<br/>
+		<ul>
+			<li>Santoro: Cerasi MariaGrazia  cerasi.mariagrazia.dsp@icannacelli.edu.it</li>
+		</ul>
+	</AccordionItem>
+
+	<AccordionItem active header="Mio figlio è positivo, che devo fare?">
+		Invia il risultato del tampone alla referente Covid
+	</AccordionItem>
+
+	<AccordionItem active header="C'è un positivo in classe: che devo fare?">
+		Fai fare a tuo figlio un tampone rapido antigenico e manda il risultato alla referente Covid.
+		Talvolta la referente non fa in tempo a contattare tutti, quindi se l'aiuti facendolo non appena
+		lo sai, le semplifichi il lavoro.
+	</AccordionItem>
+	
+	<AccordionItem active header="La referente covid mi ha mandato una email dicendo di fare il tampone">
+		Fallo e mandale il risultato.
+	</AccordionItem>
+	
+	<AccordionItem active header="Tampone T0 quale deve essere?">
+		Rapido antigenico
+	</AccordionItem>
+
+	<AccordionItem active header="Tampone T5 quale deve essere?">
+		Rapido antigenico
+	</AccordionItem>
+
+	<AccordionItem header="Serve la ricetta?">
+		No
+	</AccordionItem>
+
+
+
+	
+
+	(per esempio in farmacia piazza Balsamo Crivelli)
+
+	Rapido antigenico (per esempio in farmacia piazza Balsamo Crivelli)
+
+	<AccordionItem active header="Il tampone T0 e T5 che tipo sono">
+		Rapido antigenico (per esempio in farmacia piazza Balsamo Crivelli)
+	</AccordionItem>
+
+	<AccordionItem header="Library">
+		UCSB Library
+	</AccordionItem>
+</Accordion>
+
+
+
