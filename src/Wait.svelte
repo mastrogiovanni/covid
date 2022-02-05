267ec86922d2aa3d35011902f0198c201bc03ffd
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2022-02-05 20:00:21 +0100

    Fix

---
+++ src/Wait.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Alert } from "sveltestrap";
+</script>
+
+<Alert color="danger">
+	<h1>Guida rapida per la gestione Covid a scuola</h1>
+	<small>Siamo lavorando per aggiornare il sito in base alle nuove regole ministeriali</small>
+</Alert>
